6287d45c952361c7969838c2298c91a7b1e4edeb
Jisu Kim <jisu.kim@flatironschool.com>
2014-06-05 16:54:28 -0400

    Add animation to glyphicon tag

--- app/assets/javascripts/monkey.js
+++ app/assets/javascripts/monkey.js
@@ -8,10 +8,12 @@ $(document).ready(function(){
   });
 
   $('.glyphicon-tag').on('mouseenter',function(){
+    $(this).animate({top: '-10px'},'slow');
     $(this).closest('.tour').find('.details').fadeIn('slow');
   });
 
   $('.glyphicon-tag').on('mouseleave',function(){
+    $(this).animate({top: '0px'},'slow');
     $(this).closest('.tour').find('.details').fadeOut('slow')
   });
 });
